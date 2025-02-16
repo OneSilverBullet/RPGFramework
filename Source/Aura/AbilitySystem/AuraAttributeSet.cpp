0e4b17d67369de16b31ab3377da933e7d27751a8
Julien Zhang <silveralex2023820@gmail.com>
2025-02-16 00:10:35 -0500

    #0 update the attribute set, add vital attributes

--- Source/Aura/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/AbilitySystem/AuraAttributeSet.cpp
@@ -2,4 +2,44 @@
 
 
 #include "AuraAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include <Net/UnrealNetwork.h>
 
+UAuraAttributeSet::UAuraAttributeSet() {
+
+}
+
+/*
+* (1) The server invoke this function to gather all parameters that need to be replicated to clients.
+* (2) If the server will update the values, it will send the new values to the clients.
+* (3) The client will call OnRep_Health, OnRep_MaxHealth, OnRep_Mana, OnRep_MaxMana on the clients
+*/
+void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Health, OldHealth);
+}
+
+void UAuraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UAuraAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Mana, OldMana);
+}
+
+void UAuraAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxMana, OldMaxMana);
+}
